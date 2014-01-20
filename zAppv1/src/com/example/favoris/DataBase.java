df9891c86e7e9002a57d590fa1f5f7792a09dbe5
thierrydaireaux <thierry.daireaux@gmail.com>
2014-01-20 17:42:02 +0100

    database et preview

---
+++ zAppv1/src/com/example/favoris/DataBase.java
@@ -0,0 +1,23 @@
+package com.example.favoris;
+
+public class DataBase {
+	
+	public static String id;
+	public static boolean favorite;
+	public DataBase(){
+		
+	}
+	public static String getId() {
+		return id;
+	}
+	public static void setId(String id) {
+		DataBase.id = id;
+	}
+	public static boolean isFavorite() {
+		return favorite;
+	}
+	public static void setFavorite(boolean favorite) {
+		favorite = favorite;
+	}
+	
+}
